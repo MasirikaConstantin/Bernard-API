efed0885717e4b958bad63d4532dfb31b3ea4e91
Masirika Constantin <elconstant73@gmail.com>
2025-07-08 18:01:33 +0100

    Ajout de nouveaux fichiers JavaScript pour la gestion des erreurs et des icônes, suppression de fichiers obsolètes, mise à jour des imports et des routes, et renommage de plusieurs fichiers pour une meilleure organisation.

---
+++ public/build/assets/index-xJSbqIUv.js
@@ -0,0 +1,1 @@
+import{r,g as t}from"./app-DSk4Q1vt.js";var a=r();const e=t(a);export{e as R,a as r};
---
+++ public/build/assets/input-error-lcdobWf_.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-DSk4Q1vt.js";import{c as o}from"./app-logo-icon-BqX1L-Ma.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
